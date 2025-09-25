8cb00361be23cfd42926a89a7014fd2bcf25fbb2
belkisguler <115102181+belkisguler@users.noreply.github.com>
2025-09-25 15:54:53 +0200

    Update load_bronze.sql

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -15,7 +15,7 @@ BEGIN
 		TRUNCATE TABLE  bronze.crm_cust_info; 
 		PRINT '>>> Inserting Data into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\belki\Desktop\sql-ultimate-course\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'sql-ultimate-course\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -29,7 +29,7 @@ BEGIN
 		TRUNCATE TABLE  bronze.crm_prd_info; 
 		PRINT '>>> Inserting Data into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\belki\Desktop\sql-ultimate-course\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'sql-ultimate-course\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -43,7 +43,7 @@ BEGIN
 		TRUNCATE TABLE  bronze.crm_sales_details; 
 		PRINT '>>> Inserting Data into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\belki\Desktop\sql-ultimate-course\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'sql-ultimate-course\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -60,7 +60,7 @@ BEGIN
 		TRUNCATE TABLE  bronze.erp_cust_az12; 
 		PRINT '>>> Inserting Data into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\belki\Desktop\sql-ultimate-course\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'sql-ultimate-course\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -74,7 +74,7 @@ BEGIN
 		TRUNCATE TABLE  bronze.erp_loc_a101; 
 		PRINT '>>> Inserting Data into: bronze.erp_loc_a101';
 		BULK INSERT  bronze.erp_loc_a101
-		FROM 'C:\Users\belki\Desktop\sql-ultimate-course\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM 'sql-ultimate-course\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -88,7 +88,7 @@ BEGIN
 		TRUNCATE TABLE  bronze.erp_px_cat_g1v2;
 		PRINT '>>> Inserting Data into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\belki\Desktop\sql-ultimate-course\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'sql-ultimate-course\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
